5d76d68193de75e19262d43b5cd0a12e5b8c2074
8bitbran <brandonmar97@gmail.com>
2020-12-05 00:35:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userURL = "http://localhost:3000/users";
+    let formData = {
+        name: userName,
+        email: userEmail
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch(userURL, configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+}
