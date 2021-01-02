dffde4f2d50499fef12aaa075b0c902a9a1ee145
Daniel <danielcarvalhovr@hotmail.com>
2021-01-02 13:15:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+
+function submitData(name, email) {
+    let url = 'http://localhost:3000/users';
+
+let configObj = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        name, 
+        email
+    })
+};
+    return fetch(url, configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object['id']
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+      
+}
+
